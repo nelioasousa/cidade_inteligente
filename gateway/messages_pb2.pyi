99dd29ba28e5aaa4293f6557f318554c474ba850
nelioasousa <corneliosousaphb@gmail.com>
2025-06-21 20:15:41 -0300

    protoc messages.proto

--- gateway/messages_pb2.pyi
+++ gateway/messages_pb2.pyi
@@ -1,9 +1,38 @@
+from google.protobuf.internal import enum_type_wrapper as _enum_type_wrapper
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
 from collections.abc import Mapping as _Mapping
 from typing import ClassVar as _ClassVar, Optional as _Optional, Union as _Union
 
 DESCRIPTOR: _descriptor.FileDescriptor
+
+class RequestType(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
+    __slots__ = ()
+    REQUESTTYPE_UNSPECIFIED: _ClassVar[RequestType]
+    REQUESTTYPE_ACTION: _ClassVar[RequestType]
+    REQUESTTYPE_GET_STATE: _ClassVar[RequestType]
+    REQUESTTYPE_SET_STATE: _ClassVar[RequestType]
+    REQUESTTYPE_GET_METADATA: _ClassVar[RequestType]
+    REQUESTTYPE_SET_METADATA: _ClassVar[RequestType]
+
+class Status(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
+    __slots__ = ()
+    STATUS_UNSPECIFIED: _ClassVar[Status]
+    STATUS_OK: _ClassVar[Status]
+    STATUS_FAIL: _ClassVar[Status]
+    STATUS_DENIED: _ClassVar[Status]
+    STATUS_UNKNOWN: _ClassVar[Status]
+REQUESTTYPE_UNSPECIFIED: RequestType
+REQUESTTYPE_ACTION: RequestType
+REQUESTTYPE_GET_STATE: RequestType
+REQUESTTYPE_SET_STATE: RequestType
+REQUESTTYPE_GET_METADATA: RequestType
+REQUESTTYPE_SET_METADATA: RequestType
+STATUS_UNSPECIFIED: Status
+STATUS_OK: Status
+STATUS_FAIL: Status
+STATUS_DENIED: Status
+STATUS_UNKNOWN: Status
 
 class Address(_message.Message):
     __slots__ = ("ip", "port")
@@ -40,13 +69,29 @@ class JoinReply(_message.Message):
     def __init__(self, report_address: _Optional[_Union[Address, _Mapping]] = ..., report_interval: _Optional[int] = ...) -> None: ...
 
 class SensorReading(_message.Message):
-    __slots__ = ("sensor_name", "reading_value", "timestamp", "metadata")
+    __slots__ = ("sensor_name", "reading_value", "timestamp")
     SENSOR_NAME_FIELD_NUMBER: _ClassVar[int]
     READING_VALUE_FIELD_NUMBER: _ClassVar[int]
     TIMESTAMP_FIELD_NUMBER: _ClassVar[int]
-    METADATA_FIELD_NUMBER: _ClassVar[int]
     sensor_name: str
     reading_value: str
     timestamp: str
-    metadata: str
-    def __init__(self, sensor_name: _Optional[str] = ..., reading_value: _Optional[str] = ..., timestamp: _Optional[str] = ..., metadata: _Optional[str] = ...) -> None: ...
+    def __init__(self, sensor_name: _Optional[str] = ..., reading_value: _Optional[str] = ..., timestamp: _Optional[str] = ...) -> None: ...
+
+class DeviceRequest(_message.Message):
+    __slots__ = ("type", "key", "value")
+    TYPE_FIELD_NUMBER: _ClassVar[int]
+    KEY_FIELD_NUMBER: _ClassVar[int]
+    VALUE_FIELD_NUMBER: _ClassVar[int]
+    type: RequestType
+    key: str
+    value: str
+    def __init__(self, type: _Optional[_Union[RequestType, str]] = ..., key: _Optional[str] = ..., value: _Optional[str] = ...) -> None: ...
+
+class DeviceReply(_message.Message):
+    __slots__ = ("status", "result")
+    STATUS_FIELD_NUMBER: _ClassVar[int]
+    RESULT_FIELD_NUMBER: _ClassVar[int]
+    status: Status
+    result: str
+    def __init__(self, status: _Optional[_Union[Status, str]] = ..., result: _Optional[str] = ...) -> None: ...
