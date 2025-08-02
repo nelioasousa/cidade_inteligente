d93f235abf4f1d558298d612ef096689cab71ef9
Cornélio A. de Sousa <corneliosousaphb@gmail.com>
2025-08-02 20:46:20 -0300

    Remover código de terceiros (#1)

--- examples/NodeJS-parte1/nslookup.js
+++
@@ -1,9 +0,0 @@
-const dns = require('dns')
-dns.resolve('www.globo.com', callback=(err, result) => {
-
-if (err) {
-    console.error(`erro: ${err}`)
-  } else {
-    console.log(result)
-  }
-});
--- examples/Python-parte1/nslookup.py
+++
@@ -1,7 +0,0 @@
-import socket
-site = "globo.com"
-try:
-    ip = socket.gethostbyname(site)
-    print(ip)
-except:
-    print("Endereço não encontrado")
